5e29f8045fa23a53ed05b2b7620eb16f1023d686
mintesnot lemma <mintesnotlemma@gmail.com>
2018-12-03 11:00:17 -0500

    challenges code

---
+++ hellojs/variable.js
@@ -0,0 +1,28 @@
+//variable.js
+
+var1 = 1;  // don not do this. this is varible is boundles. functions scope and document scope. global and local scope. global to what.
+var var2 = 2;  // document scope.
+let var3 = 3;  // let creates its own scope and it does change.  fucntion scope
+ const var4 = 4;  // creates itw own scope doesnot change.
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(var4);
+
+var1 = "html";  // don not do this. this is varible is boundles. functions scope and document scope. global and local scope. global to what.
+var2 = "CSS";  // document scope.
+var3 = "Lemma";  // let creates its own scope and it does change.  fucntion scope
+//var4 = "Mintesnot";
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(var4);
+
+var var5 = 1000;
+let var5;
+
+
+
+
---
+++ hellojs/variable2.js
@@ -0,0 +1,10 @@
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(var4);
+
+var1 = 'I am the first';
+
+
+
